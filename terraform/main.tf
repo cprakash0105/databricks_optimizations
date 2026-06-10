43afe7bdfdcb23c76d68052b1e2542d32315a18c
Chandra Prakash (QMN R) <chandra.prakash@bt.com>
2026-06-10 11:28:04 +0530

    fix: update backend storage account name from bootstrap

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate"
+    storage_account_name = "stterraformstateddc7b427"
     container_name       = "tfstate"
     key                  = "databricks-optimizations/terraform.tfstate"
   }
